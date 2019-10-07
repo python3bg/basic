b1fd302282f6b290cd423539b344e354fc1f7767
Benny Gu <i040351@pvgl56594150b.apj.global.corp.sap>
2019-10-07 16:46:03 +0800

    update

--- hello_world.py
+++ hello_world.py
@@ -2,5 +2,14 @@
 message = "Hello Python word"
 print(message)
 
-message = "Hello Python Crash Course world!"
+message = "! Hello Python Crash Course world! "
 print(message)
+print(message.lower())
+print("(message.upper()"+message.upper())
+print(message.lower().count('cr'))
+print("message.count('C')"+str(message.count('C')))
+print('len(message)'+str(len(message)))
+print("str(message.count(' '))"+str(message.count(' ')))
+print('with strip'+str(message.strip().count(' ')))
+print('find !' + str(message.find("!")))
+print('replace! to ^:' + message.replace('!', '^'))
