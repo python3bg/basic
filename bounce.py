369024a714577f6c4e1d498b95d39a71aa6e230e
Benny Gu <i040351@pvgl56594150b.apj.global.corp.sap>
2019-10-07 18:05:45 +0800

    update

--- bounce.py
+++ bounce.py
@@ -1,26 +1,28 @@
-import sys, pygame
 
+
+import pygame
+import sys
 pygame.init()
 
 size = width, height = 640, 480
 dx = 1
 dy = 1
-x= 163
+x = 163
 y = 120
-black = (0,0,0)
-white = (255,255,255)
-
+black = (0, 0, 0)
+white = (255, 255, 255)
 screen = pygame.display.set_mode(size)
 
 while 1:
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
     x += dx
     y += dy
 
-    if x < 0 or x > width:   
+    if x < 0 or x > width:
         dx = -dx
 
     if y < 0 or y > height:
@@ -28,6 +30,12 @@ while 1:
 
     screen.fill(black)
 
-    pygame.draw.circle(screen, white, (x,y), 8)
+    pygame.draw.circle(screen, white, (x, y), 8)
 
     pygame.display.flip()
+
+a = 1
+b = 2
+c = 3
+
+d = a+b+c
