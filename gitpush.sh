1345de3add31cf2f4accb1a1800d863b93e796a3
Benny Gu <i040351@pvgl56594150b.apj.global.corp.sap>
2019-10-15 16:40:52 +0800

    empty_commit_message

--- gitpush.sh
+++ gitpush.sh
@@ -1,3 +1,10 @@
-git add .
-git commit -m update
+#!/bin/bash
+
+# git config --global user.name "$(getent passwd $USER | cut -d ':' -f 5 | cut -d ',' -f 1)"
+# git config --global user.email "benny.gu@sap.com"
+if [ $# -eq 0 ];then
+git commit -am "empty_commit_message"
+else
+git commit -am "$*"
+fi
 git push
